51a56ba91739b35a05b063aab1dc01f34e64f8a0
1110646 <yogibearysk1008@gmail.com>
2025-05-29 13:06:11 -0400

    public cors headers for testing

--- pages/api/init-users.js
+++ pages/api/init-users.js
@@ -8,7 +8,7 @@ const supabase = createClient(
 export default async function handler(req, res) {
   
   res.setHeader("Access-Control-Allow-Credentials", "true")
-  res.setHeader("Access-Control-Allow-Origin", "https://strategic-embeds-177144.framer.app")
+  res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT")
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
